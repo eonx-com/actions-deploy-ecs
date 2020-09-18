c464bbfb7b73897856aa96127b6c191d8bd1c812
Damian Sloane <damian@capacitorlabs.com>
2020-09-18 14:53:31 +1000

    Work in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# If a Github token was supplied use that
+if [[ ! -z "${DOCKER_BASE_IMAGE_REPOSITORY}" && ! -z "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME}" && ! -z "${DOCKER_BASE_IMAGE_REPOSITORY_PASSWORD}" && ! -z "${DOCKER_BASE_IMAGE}" ]]; then
+  echo "Authenticating: ${DOCKER_BASE_IMAGE_REPOSITORY}..."
+  docker login "${DOCKER_BASE_IMAGE_REPOSITORY}" \
+    --username "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME}" \
+    --password "${DOCKER_BASE_IMAGE_REPOSITORY_PASSWORD}";
+  echo "Pulling: ${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}..."
+  docker pull "${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}"
+fi
+
 export ENVIRONMENT=${1}
 export IMAGE_TAG=${2}
 export PYTHONUNBUFFERED=TRUE
