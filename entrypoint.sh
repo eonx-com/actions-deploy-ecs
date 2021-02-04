10f9422e6be0cff2dce54f331943e4c4e2839eb7
Damian Sloane <damian@capacitorlabs.com>
2021-02-04 17:29:05 +1100

    Work in progress

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 
-# If a base image is required login to the Docker repository and pull the image first
-if [[ ! -z "${DOCKER_BASE_IMAGE_REPOSITORY}" && ! -z "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME}" && ! -z "${DOCKER_BASE_IMAGE_REPOSITORY_PASSWORD}" && ! -z "${DOCKER_BASE_IMAGE}" ]]; then
-  echo "Authenticating: ${DOCKER_BASE_IMAGE_REPOSITORY}..."
-  docker login "${DOCKER_BASE_IMAGE_REPOSITORY}" \
-    --username "${DOCKER_BASE_IMAGE_REPOSITORY_USERNAME}" \
-    --password "${DOCKER_BASE_IMAGE_REPOSITORY_PASSWORD}";
-  echo "Pulling: ${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}..."
-  docker pull "${DOCKER_BASE_IMAGE_REPOSITORY}/${DOCKER_BASE_IMAGE}"
-fi
-
-export ENVIRONMENT=${1}
-export IMAGE_TAG=${2}
+export ECS_CLUSTER_NAME=${1}
+export ECS_SERVICE_NAME=${2}
+export ECS_TASK_NAME=${3}
+export ENVIRONMENT=${4}
+export IMAGE_TAG=${5}
 
 # Disable Python buffering so that output appears in GitHub Action during execution (otherwise it sits silent until the very end when it dumps the entire log)
 export PYTHONUNBUFFERED=TRUE
